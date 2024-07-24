f9643ca3f88159a08beef28df0accfc6bca6dc7d
steve <steven.uwague@outlook.de>
2024-07-24 14:38:19 +0200

    added mm

---
+++ object_detection/darwin_to_yolo.py
@@ -0,0 +1,8 @@
+from darwin.cli import convert_annotations
+
+convert_annotations(
+    input_format="darwin",
+    output_format="yolo",
+    files=["C:/Users/steve/Downloads/keypoints/p1_24_06_1744.json"],
+    output_directory=""
+)
